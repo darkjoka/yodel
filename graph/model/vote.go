91179497c58418b5c950f39275201a8578d189d9
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2022-08-10 01:41:00 +0000

    add vote model

---
+++ graph/model/vote.go
@@ -0,0 +1,15 @@
+package model
+
+import (
+	"github.com/google/uuid"
+	"github.com/uptrace/bun"
+)
+
+type Vote struct {
+	bun.BaseModel `bun:"table:vote,alias:v"`
+
+	ID       uuid.UUID `bun:"id,type:uuid,pk,default:uuid_generate_v4()"`
+	UserID   uuid.UUID `bun:"user_id,type:uuid"`
+	postID   uuid.UUID `bun:"post_id,type:uuid"`
+	IsUpvote bool      `bun:"is_upvote,notnull"`
+}
