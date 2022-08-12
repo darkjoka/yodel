dc7ad4684078933e57ff4bfd90c168b75d696750
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2022-08-12 05:28:51 +0000

    add commentor creation func

--- graph/model/commentor.go
+++ graph/model/commentor.go
@@ -1,6 +1,8 @@
 package model
 
 import (
+	"context"
+
 	"github.com/google/uuid"
 	"github.com/uptrace/bun"
 )
@@ -12,4 +14,13 @@ type Commentor struct {
 	UserID      uuid.UUID `bun:"user_id,type:uuid,unique"`
 	postID      uuid.UUID `bun:"post_id,type:uuid"`
 	IncrementID int       `bun:"increment_id,autoincrement"`
+}
+
+type CommentorScheme struct {
+	DB *bun.DB
+}
+
+func (c *CommentorScheme) Create(commentor *Commentor, ctx context.Context) error {
+	_, err := c.DB.NewInsert().Model(commentor).Exec(ctx)
+	return err
 }
