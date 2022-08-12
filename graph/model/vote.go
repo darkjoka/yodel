5221b7b0eff3b618a6f1b9d7848635a9af978b41
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2022-08-12 05:43:18 +0000

    add vote funcs

--- graph/model/vote.go
+++ graph/model/vote.go
@@ -1,6 +1,8 @@
 package model
 
 import (
+	"context"
+
 	"github.com/google/uuid"
 	"github.com/uptrace/bun"
 )
@@ -12,4 +14,24 @@ type Vote struct {
 	UserID   uuid.UUID `bun:"user_id,type:uuid"`
 	postID   uuid.UUID `bun:"post_id,type:uuid"`
 	IsUpvote bool      `bun:"is_upvote,notnull"`
+}
+
+type VoteScheme struct {
+	DB *bun.DB
+}
+
+func (v *VoteScheme) Create(vote *Vote, ctx context.Context) error {
+	_, err := v.DB.NewInsert().Model(vote).Exec(ctx)
+	return err
+}
+
+func (v *VoteScheme) Delete(vote *Vote, ctx context.Context) error {
+	_, err := v.DB.NewDelete().Model(vote).WherePK().Exec(ctx)
+	return err
+}
+
+func (v *VoteScheme) FindOne(user, post uuid.UUID, ctx context.Context) error {
+	vote := new(Vote)
+	err := v.DB.NewSelect().Model(vote).Where("user_id = ? AND post_id = ?", user, post).Scan(ctx)
+	return err
 }
