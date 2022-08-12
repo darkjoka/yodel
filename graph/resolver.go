940609f293a2fabcdc99d84e15e9b93aa0b88124
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2022-08-12 05:12:04 +0000

    resolver types

--- graph/resolver.go
+++ graph/resolver.go
@@ -1,7 +1,12 @@
 package graph
 
+import "github.com/darkjoka/yodel/graph/model"
+
 // This file will not be regenerated automatically.
 //
 // It serves as dependency injection for your app, add any dependencies you require here.
 
-type Resolver struct{}
+type Resolver struct {
+	UserScheme model.UserScheme
+	PostScheme model.PostScheme
+}
