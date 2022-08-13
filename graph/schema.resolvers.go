173e6075f6d89d402ea6939158a205421f9ae3a6
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2022-08-13 16:38:30 +0000

    add new comment resolver

--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -5,8 +5,10 @@ package graph
 
 import (
 	"context"
+	"errors"
 	"fmt"
 
+	"github.com/darkjoka/yodel/graph/auth"
 	"github.com/darkjoka/yodel/graph/generated"
 	"github.com/darkjoka/yodel/graph/jwt"
 	"github.com/darkjoka/yodel/graph/model"
@@ -33,7 +35,7 @@ func (r *commentResolver) OrderIndex(ctx context.Context, obj *model.Comment) (i
 	commentor := new(model.Commentor)
 	query := r.CommentorScheme.DB.NewSelect().Model(commentor).Where("user_id = ? AND post_id = ?", obj.UserID, obj.PostID)
 	if err := query.Scan(ctx); err == nil {
-		return commentor.IncrementID, nil
+		return commentor.Order, nil
 	}
 
 	// post author is commentor
@@ -63,10 +65,14 @@ func (r *mutationResolver) Login(ctx context.Context, input model.NewUser) (stri
 // NewPost is the resolver for the newPost field.
 func (r *mutationResolver) NewPost(ctx context.Context, input model.NewPost) (*model.Post, error) {
 	// TODO: run validations against input
-	userId, _ := uuid.Parse(input.UserID)
+
+	user, ok := auth.ForContext(ctx)
+	if !ok {
+		return nil, errors.New("Access denied")
+	}
 
 	post := &model.Post{
-		UserID:    userId,
+		UserID:    user.ID,
 		Content:   input.Content,
 		Longitude: float32(input.Longitude),
 		Latitude:  float32(input.Latitude),
@@ -81,28 +87,61 @@ func (r *mutationResolver) NewPost(ctx context.Context, input model.NewPost) (*m
 func (r *mutationResolver) NewComment(ctx context.Context, input *model.NewComment) (*model.Comment, error) {
 	// TODO: post_id should be change to parent id and type possibly added.
 	// TODO: after jwt integration remove user id from input and refine author check
-	userId, _ := uuid.Parse(input.UserID)
-	postId, _ := uuid.Parse(input.PostID)
+
+	user, ok := auth.ForContext(ctx)
+
+	if !ok {
+		return nil, errors.New("Access denied")
+	}
+	postID, _ := uuid.Parse(input.PostID)
 
 	// get post author and check if the same as commentor
 	post := new(model.Post)
-	r.PostScheme.DB.NewSelect().Model(post).Where("id = ?", postId).Scan(ctx)
+	r.PostScheme.DB.
+		NewSelect().
+		Model(post).
+		Where("id = ?", postID).
+		Scan(ctx)
+
 	// not author
-	if post.UserID != userId {
+	if post.UserID != user.ID {
 		commentor := new(model.Commentor)
-		query := r.CommentorScheme.DB.NewSelect().Model(commentor).Where("user_id = ? AND post_id = ?", userId, postId)
+		query := r.CommentorScheme.DB.
+			NewSelect().
+			Model(commentor).
+			Where("user_id = ? AND post_id = ?", user.ID, postID)
+
+		// commentor has no previous comment
 		if err := query.Scan(ctx); err != nil {
+			// var highestOrder int
+			r.CommentorScheme.DB.
+				NewSelect().
+				Model(commentor).
+				Order("order_id DESC").
+				Limit(1).
+				Scan(ctx)
+			// Model(highestOrder).
+			// Where("post_id = ?", postID).
+			// Scan(ctx)
+
+			fmt.Println(commentor)
+
 			newCommentor := &model.Commentor{
-				UserID: userId,
-				PostID: postId,
+				UserID: user.ID,
+				PostID: postID,
+				Order:  commentor.Order + 1,
 			}
-			r.CommentScheme.DB.NewInsert().Model(newCommentor).Exec(ctx)
+
+			r.CommentorScheme.DB.
+				NewInsert().
+				Model(newCommentor).
+				Exec(ctx)
 		}
 	}
 
 	comment := &model.Comment{
-		UserID:    userId,
-		PostID:    postId,
+		UserID:    user.ID,
+		PostID:    postID,
 		Content:   input.Content,
 		Longitude: float32(input.Longitude),
 		Latitude:  float32(input.Latitude),
@@ -110,8 +149,9 @@ func (r *mutationResolver) NewComment(ctx context.Context, input *model.NewComme
 	}
 
 	_, err := r.CommentScheme.DB.NewInsert().Model(comment).Exec(ctx)
-	// TODO: validate before output
+	// // TODO: validate before output
 	return comment, err
+
 }
 
 // ID is the resolver for the id field.
