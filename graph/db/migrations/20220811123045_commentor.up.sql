c03b5df63012a32fbd342c7f118f73df55cef9d2
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2022-08-13 14:28:38 +0000

    modified db schema

--- graph/db/migrations/20220811123045_commentor.up.sql
+++ graph/db/migrations/20220811123045_commentor.up.sql
@@ -3,5 +3,5 @@ CREATE TABLE
         id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
         user_id UUID NOT NULL,
         post_id UUID NOT NULL,
-        increment_id SERIAL
+        order_id SMALLINT NOT NULL
     )
