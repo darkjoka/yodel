806688ff4ebbe5dbaf814986f5745a895cfb590b
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2022-08-13 16:38:50 +0000

    add auth middelware

--- server.go
+++ server.go
@@ -8,10 +8,13 @@ import (
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/darkjoka/yodel/graph"
+	"github.com/darkjoka/yodel/graph/auth"
 	"github.com/darkjoka/yodel/graph/db"
 	"github.com/darkjoka/yodel/graph/generated"
 	"github.com/darkjoka/yodel/graph/model"
+	"github.com/go-chi/chi/v5"
 	"github.com/joho/godotenv"
+	"github.com/uptrace/bun/extra/bundebug"
 )
 
 const defaultPort = "8000"
@@ -32,19 +35,26 @@ func main() {
 	DB := db.New(dsn)
 	defer DB.Close()
 
+	// Print all queries to stdout.
+	DB.AddQueryHook(bundebug.NewQueryHook(bundebug.WithVerbose(true)))
+
+	UserScheme := model.UserScheme{DB: DB}
+	router := chi.NewRouter()
+	router.Use(auth.Middleware(UserScheme))
+
 	cfg := &graph.Resolver{
-		UserScheme:      model.UserScheme{DB: DB},
+		UserScheme:      UserScheme,
 		PostScheme:      model.PostScheme{DB: DB},
 		CommentScheme:   model.CommentScheme{DB: DB},
 		VoteScheme:      model.VoteScheme{DB: DB},
-		CommentorScheme: model.CommentorScheme{},
+		CommentorScheme: model.CommentorScheme{DB: DB},
 	}
 
 	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: cfg}))
 
-	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", srv)
+	router.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	router.Handle("/query", srv)
 
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	log.Fatal(http.ListenAndServe(":"+port, router))
 }
